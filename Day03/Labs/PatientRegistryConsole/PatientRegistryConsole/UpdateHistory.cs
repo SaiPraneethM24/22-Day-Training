27eae9eb5d29e6d7fa3a89e939b3a08aae0aae92
Mangipudi Sai Praneeth <praneethmangipudi@gmail.com>
2026-06-08 23:10:40 +0530

    Day 03 - Labs and Assignments

---
+++ Day03/Labs/PatientRegistryConsole/PatientRegistryConsole/UpdateHistory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatientRegistryConsole
+{
+    internal class UpdateHistory
+    {
+        public string OldValue { get; set; }
+        public string NewValue { get; set; }
+        public DateTime Timestamp { get; set; }
+        public UpdateHistory()
+        {
+            Timestamp = DateTime.Now;
+        }
+        public string ToString()
+        {
+            return $"[{Timestamp:yyyy-MM-dd HH:mm:ss}] {OldValue} -> {NewValue}";
+        }
+    }
+}
